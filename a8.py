35b3527a64ab5a0d1eda7e6b304e2ae6e52bc9cf
Your Name <you@example.com>
2024-04-19 10:20:59 -0500

    Working on A8

--- a8.py
+++ a8.py
@@ -2,6 +2,10 @@ from neural import *
 
 print("<<<<<<<<<<<<<< XOR >>>>>>>>>>>>>>\n")
 
+#Training Data
+print("\n\nTraining XOR\n\n")
+xor_training_data = [([1, 1], [0]), ([1, 0], [1]), ([0, 1], [1]), ([0, 0], [0])]
 
-
-
+xorn = NeuralNet(2, 1, 1)
+xorn.train(xor_training_data)
+print(xorn.test_with_expected(xor_training_data))
