ee7c3f08abc713601a9801a10665a8e7e8f9b94e
Your Name <you@example.com>
2024-04-19 10:37:09 -0500

    Working on A8

--- a8.py
+++ a8.py
@@ -9,3 +9,4 @@ xor_training_data = [([1, 1], [0]), ([1, 0], [1]), ([0, 1], [1]), ([0, 0], [0])]
 xorn = NeuralNet(2, 1, 1)
 xorn.train(xor_training_data)
 print(xorn.test_with_expected(xor_training_data))
+print(xorn.get_ho_weights())
